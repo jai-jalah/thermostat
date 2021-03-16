1e36b39fcc6d3c40879577647f915b3b302e696c
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-03-16 23:16:53 +0000

    Edits

--- interface.js
+++ interface.js
@@ -41,3 +41,23 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   };
 });
+
+$.get('{api_key}', function(data) {
+$('#current-temperature').text(data.main.temp);
+
+displayWeather('London');
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+})
+
+function displayWeather(city) {
+ var url = '{api_key}' + city;
+ var token = '{api_token}';
+ var units = '&units=metric';
+ $.get(url + token + units, function(data) {
+   $('#current-temperature').text(data.main.temp);
+ })
+})
